4702f13e9fa7b604e66c287da868e2499d4e33fd
alinaiagapowa <alinaiagapowa@gmail.com>
2024-07-29 17:02:23 +0300

    4.3_4 added test_product_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -6,4 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-    
+
+class ProductPageLocators():
+    ADD_PROD_1 = (By.CSS_SELECTOR, '#add_to_basket_form > button')
+    TEXT_ADD_BASKET = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
+    TEXT_PROD = (By.CSS_SELECTOR, '.col-sm-6.product_main > h1')
+
+    AMOUNT_BASKET = (By.CSS_SELECTOR, '.alert-info.fade.in > div > p:nth-child(1) > strong')
